ded62ef0209d083155d060f73ca68ea98082aa93
Valentine Mayaki <mxvmayaki@gmail.com>
2019-03-21 09:33:26 +0100

    Allowed  indirect call link method in connector

--- app/Core/Database/MysqlConnector.php
+++ app/Core/Database/MysqlConnector.php
@@ -44,4 +44,33 @@ class MysqlConnector
         }
         return static::$_instance;
     }
+    /**
+     * Check if a table exists in the current database.
+     *
+     * @param PDO $pdo PDO instance connected to a database.
+     * @param string $table Table to search for.
+     * @return bool TRUE if table exists, FALSE if no table found.
+     */
+    function tableExists($table) {
+
+        // Try a select statement against the table
+        // Run it in try/catch in case PDO is in ERRMODE_EXCEPTION.
+        try {
+            $result = $this->link->query("SELECT 1 FROM $table LIMIT 1");
+        } catch (\Exception $e) {
+            // We got an exception == table not found
+            return FALSE;
+        }
+
+        // Result is either boolean FALSE (no table found) or PDOStatement Object (table found)
+        return $result !== FALSE;
+    }
+    function __call($name, $arguments)
+    {
+        if(method_exists($this->link, $name)){
+            return \call_user_func_array([$this->link, $name], $arguments);
+        }
+        return $this->{$name}(...$arguments);
+    }
+
 }
